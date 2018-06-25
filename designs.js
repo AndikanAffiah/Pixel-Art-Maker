8b30cacf0bf1dbe6a5fef3bea2fee2ef0ca28469
enactor <kyrioxlamda@gmail.com>
2018-06-25 02:30:43 +0100

    Pixel Art Project

---
+++ designs.js
@@ -0,0 +1,47 @@
+// Select color input --done
+// Select size input --done
+// When size is submitted by the user, call makeGrid() -- done
+//callGrid will use for loop to create the elements -- done
+//get the value of the color -- done
+//select all the td elements and add eventListener to them -- done
+//if td is not created, it will give you an error, -- done
+//loop through all td for the click event and set the background to the color value
+
+var width, height, table, cells, tr, td;
+var coloured = document.getElementById('colorPicker');
+
+function makeGrid() {
+
+   width = document.getElementById('inputWeight').value; //set width value
+   height = document.getElementById('inputHeight').value; //set height value
+   table = document.getElementById('pixelCanvas');
+
+   table.innerHTML = ''; //set table to empty
+
+   for(i = 0; i < height; i++){ //no of rows to equal height value
+      tr = document.createElement('tr'); //create rows
+      
+      for(j = 0; j < width; j++){ //no of columns to equal width value
+
+         td = document.createElement('td'); //create columns
+         // td.setAttribute('id', 'td');
+         tr.appendChild(td);
+      }
+      table.appendChild(tr);
+   }
+
+   cells = document.querySelectorAll('td'); //create an array of cells
+   //console.log(cells);
+   for (var i = 0; i < cells.length; i++) { //loop over each cell
+      cells[i].onclick = function(){ //attach onclick eventListener
+         this.style.backgroundColor = coloured.value; //set cell background to coloured value
+         
+      };
+   }
+
+   
+}
+
+
+
+
